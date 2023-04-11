482f381a29a792c088d89b588681bf8ebdcfb57c
1chori <pro4456@naver.com>
2023-04-11 12:03:24 +0900

    0411am

--- myPage.js
+++ myPage.js
@@ -1,31 +1,43 @@
 let nickName = document.querySelector('.accessInfo span');
 let changNicBtn = document.querySelector('.changeNic');
-let checkBtn = document.querySelector('.nickName button');
-
+let subBtn = document.querySelector('.submit');
+let closeBtn = document.querySelector('.close');
 let nickPopup = document.querySelector('.nickName');
 
-
-// changNicBtn.addEventListener('click', () => {
-//     nickPopup.classList.add('active');
-// })
-
-// 닉네임 변경 팝업창 활성화
+// 닉네임 변경 팝업창 활성화하는 함수
 function popup() {
     document.body.classList.toggle('active');
     nickPopup.classList.toggle('active');
 }
 
+// 닉네임 팝업창 close
+closeBtn.addEventListener('click', () => {
+    document.body.classList.remove('active');
+    nickPopup.classList.remove('active');
+})
 
-
+// 닉네임 변경 진행하는 함수
 function isNickname(input) {
-    const regex = /^[A-Za-z0-9]{2,12}$/;
+    const regex = /^[\uAC00-\uD7A3A-Za-z0-9]{2,12}$/;
     return regex.test(input);
 }
 
-checkBtn.onclick = function () {
-    let userNic = document.querySelector('.userNic').value;
-    console.log(userNic);
-    if (!isNickname(userNic)) {
-        document.querySelector('.cautionText').innerHTML = '닉네임 형식이 올바르지 않습니다.<br>(최소 2자 최대 12자 및 특수문자/띄어쓰기 불가)'
+
+subBtn.onclick = function () {
+    let inputNic = document.querySelector('.inputNic').value;
+
+    if (!isNickname(inputNic)) {
+        document.querySelector('.cautionText').innerHTML = '닉네임 형식이 올바르지 않습니다.<br>(최소 2자 최대 12자 및 특수문자/띄어쓰기 불가)';
+    } else {
+        document.body.classList.remove('active');
+        nickPopup.classList.remove('active');
+        nickName.innerHTML = inputNic;
+        localStorage.setItem('nickname', inputNic);
     }
 }
+
+let storageNic = localStorage.getItem('nickname');
+if (storageNic) {
+    nickName.innerHTML = storageNic;
+}
+
