4649489145c5a6be48b5d27848eb96c80d95daf6
zerohoney <zerohoney97@gmail.com>
2023-04-18 14:18:02 +0900

    dswe

---
+++ util.js
@@ -0,0 +1,29 @@
+// ********************************************************************************************
+// *** notice: 본 함수는 프론트와 백엔드를 간단하게 이어주기위한 유틸 js파일 입니다. ***
+// ********************************************************************************************
+
+const getLocalStorage = (localStorageName,index) => {
+  try {
+    if (localStorage.getItem(localStorageName)) {
+      let tempInfo = localStorage.getItem(localStorageName);
+      if (index) {
+      return JSON.parse(tempInfo)[index];
+        
+      }
+      return JSON.parse(tempInfo);
+    } else {
+      throw new Error("로컬 스토레이지가 비었습니다!");
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const setLocalStorage = (localStorageName, data) => {
+  localStorage.setItem(localStorageName, JSON.stringify(data));
+};
+
+
+
+
+
