1992498de8cefecfefc28e74d45bcfb0bf868fbe
zerohoney <zerohoney97@gmail.com>
2023-04-25 10:09:34 +0900

    asdasd

--- trade/dist/yeon.dev.js
+++ trade/dist/yeon.dev.js
@@ -1,27 +1,26 @@
-let divBtn = document.getElementById("btn");
-let modal = document.getElementById("modal");
-let addBtn = document.getElementById("add-btn");
-let closeBtn = document.getElementById("close-btn");
-let input = document.getElementById("text");
-let subContainer = document.querySelector(".sub-container");
-let list = document.querySelector(".list");
-let textarea = document.getElementById("text2");
-let board = getLocalStorage("board");
-let tit = document.getElementById("tit");
-let titClass = document.querySelectorAll(".tit");
-let titleCon = document.getElementById("title-con");
-let conClose = document.getElementById("con-close");
-let conText = document.querySelector(".content-text");
-let delBtn = document.getElementById("delBtn");
-let deleteIndex = 0;
+"use strict";
 
-//---페이지 네이션
-let currentPage = 1;
-let totalCount = 50;
-let listCount = 12;
-let limit = 3;
-//------
+var divBtn = document.getElementById("btn");
+var modal = document.getElementById("modal");
+var addBtn = document.getElementById("add-btn");
+var closeBtn = document.getElementById("close-btn");
+var input = document.getElementById("text");
+var subContainer = document.querySelector(".sub-container");
+var list = document.querySelector(".list");
+var textarea = document.getElementById("text2");
+var board = getLocalStorage("board");
+var tit = document.getElementById("tit");
+var titClass = document.querySelectorAll(".tit");
+var titleCon = document.getElementById("title-con");
+var conClose = document.getElementById("con-close");
+var conText = document.querySelector(".content-text");
+var delBtn = document.getElementById("delBtn");
+var deleteIndex = 0; //---페이지 네이션
 
+var currentPage = 1;
+var totalCount = 50;
+var listCount = 12;
+var limit = 3; //------
 // document.querySelectorAll(".current-page").forEach((a) => {
 //   a.addEventListener("click", (e) => {
 //     currentPage = parseInt(e.target.getAttribute("value"));
@@ -45,11 +44,9 @@ let limit = 3;
 // document.querySelector("#page2").innerHTML = currentPage + 1;
 // document.querySelector("#page3").innerHTML = currentPage + 2;
 // //----제목 클릭하면 content 나오는 함수
-
 // // titClass.addEventListener("click",function(){
 // //         titleCon.style.display= "block";
 // //     })
-
 // //모든배열 요소에 이벤트 추가
 // conClose.addEventListener("click", function () {
 //   titleCon.style.display = "none";
@@ -86,19 +83,15 @@ let limit = 3;
 //     let numElement = document.createElement("div");
 //     numElement.className = "num";
 //     numElement.innerHTML = obj.num;
-
 //     let titElement = document.createElement("div");
 //     titElement.className = "tit";
 //     titElement.innerHTML = obj.title;
-
 //     let writerElement = document.createElement("div");
 //     writerElement.className = "writer";
 //     writerElement.innerHTML = obj.writer;
-
 //     // let viewElement = document.createElement("div");
 //     // viewElement.className = "view";
 //     // viewElement.innerHTML = obj.view
-
 //     _list.append(numElement);
 //     _list.append(titElement);
 //     _list.append(writerElement);
@@ -113,79 +106,91 @@ let limit = 3;
 if (board) {
   // 연수 삽질 할 곳
   if (board.length < 12) {
-    for (let i = (currentPage - 1) * 12; i < board.length; i++) {
-      let _list = document.createElement("div");
+    var _loop = function _loop(i) {
+      var _list = document.createElement("div");
+
       _list.classList.add("list");
-      let numElement = document.createElement("div");
+
+      var numElement = document.createElement("div");
       numElement.className = "num";
       numElement.innerHTML = board[i].num;
-
-      let titElement = document.createElement("div");
+      var titElement = document.createElement("div");
       titElement.className = "tit";
       titElement.innerHTML = board[i].title;
-
-      let writerElement = document.createElement("div");
+      var writerElement = document.createElement("div");
       writerElement.className = "writer";
-      writerElement.innerHTML = board[i].writer;
-
-      // let viewElement = document.createElement("div");
+      writerElement.innerHTML = board[i].writer; // let viewElement = document.createElement("div");
       // viewElement.className = "view";
       // viewElement.innerHTML = board[i].view
 
       _list.append(numElement);
+
       _list.append(titElement);
-      _list.append(writerElement);
-      // _list.append(viewElement)
-      subContainer.append(_list);
-      //중요
+
+      _list.append(writerElement); // _list.append(viewElement)
+
+
+      subContainer.append(_list); //중요
+
       titClass = document.querySelectorAll(".tit");
       titClass[i + 1].addEventListener("click", function () {
         conText.innerHTML = board[i].content;
         deleteIndex = i;
         titleCon.style.display = "block";
       });
+    };
+
+    for (var i = (currentPage - 1) * 12; i < board.length; i++) {
+      _loop(i);
     }
   } else {
-    for (let i = (currentPage - 1) * 12; i < currentPage * 12; i++) {
-      let _list = document.createElement("div");
+    var _loop2 = function _loop2(_i) {
+      var _list = document.createElement("div");
+
       _list.classList.add("list");
-      let numElement = document.createElement("div");
+
+      var numElement = document.createElement("div");
       numElement.className = "num";
-      numElement.innerHTML = board[i].num;
-
-      let titElement = document.createElement("div");
+      numElement.innerHTML = board[_i].num;
+      var titElement = document.createElement("div");
       titElement.className = "tit";
-      titElement.innerHTML = board[i].title;
-
-      let writerElement = document.createElement("div");
+      titElement.innerHTML = board[_i].title;
+      var writerElement = document.createElement("div");
       writerElement.className = "writer";
-      writerElement.innerHTML = board[i].writer;
-
-      // let viewElement = document.createElement("div");
+      writerElement.innerHTML = board[_i].writer; // let viewElement = document.createElement("div");
       // viewElement.className = "view";
       // viewElement.innerHTML = board[i].view
 
       _list.append(numElement);
+
       _list.append(titElement);
-      _list.append(writerElement);
-      // _list.append(viewElement)
-      subContainer.append(_list);
-      //중요
+
+      _list.append(writerElement); // _list.append(viewElement)
+
+
+      subContainer.append(_list); //중요
+
       titClass = document.querySelectorAll(".tit");
-      titClass[i + 1].addEventListener("click", function () {
-        conText.innerHTML = board[i].content;
-        deleteIndex = i;
+
+      titClass[_i + 1].addEventListener("click", function () {
+        conText.innerHTML = board[_i].content;
+        deleteIndex = _i;
         titleCon.style.display = "block";
       });
+    };
+
+    for (var _i = (currentPage - 1) * 12; _i < currentPage * 12; _i++) {
+      _loop2(_i);
     }
   }
 }
+
 delBtn.addEventListener("click", function (e) {
-  console.log("클릭");
-  //보드 안에 있는 특정한 key의 value를 삭제
+  console.log("클릭"); //보드 안에 있는 특정한 key의 value를 삭제
   //get으로 다 가져와=> 그 중에서 삭제할 게시판의 고유 아이디를 가진 객체를 찾아
   //그 다음 그 객체의 content를 지우고, 다시 setLocalstorage로 저장
-  let boardArr = JSON.parse(localStorage.getItem("board"));
+
+  var boardArr = JSON.parse(localStorage.getItem("board"));
   console.log(boardArr[deleteIndex]);
 });
 
@@ -196,7 +201,8 @@ function index() {
     return 1;
   }
 }
-const validatePagination = () => {
+
+var validatePagination = function validatePagination() {
   if (12 * currentPage > board.length) {
     return true;
   } else {
@@ -204,74 +210,76 @@ const validatePagination = () => {
   }
 };
 
-const changePage = () => {
+var changePage = function changePage() {
   if (validatePagination()) {
     subContainer.innerHTML = "";
-    let j = 0;
-    for (let i = (currentPage - 1) * 12; i < board.length; i++) {
-      let _list = document.createElement("div");
-      _list.classList.add("list");
-      let numElement = document.createElement("div");
-      numElement.className = "num";
-      numElement.innerHTML = board[i].num;
+    var j = 0;
 
-      //       let titElement = document.createElement("div");
+    var _loop3 = function _loop3(_i2) {
+      var _list = document.createElement("div");
+
+      _list.classList.add("list");
+
+      var numElement = document.createElement("div");
+      numElement.className = "num";
+      numElement.innerHTML = board[_i2].num; //       let titElement = document.createElement("div");
       //       titElement.className = "tit";
       //       titElement.innerHTML = board[i].title;
-
       //       let writerElement = document.createElement("div");
       //       writerElement.className = "writer";
       //       writerElement.innerHTML = board[i].writer;
-
       //       // let viewElement = document.createElement("div");
       //       // viewElement.className = "view";
       //       // viewElement.innerHTML = board[i].view
 
       _list.append(numElement);
+
       _list.append(titElement);
-      _list.append(writerElement);
-      // _list.append(viewElement)
+
+      _list.append(writerElement); // _list.append(viewElement)
+
+
       subContainer.append(_list);
       titClass = document.querySelectorAll(".tit");
       titClass[j].addEventListener("click", function () {
-        conText.innerHTML = board[i].content;
+        conText.innerHTML = board[_i2].content;
         console.log("asd");
         titleCon.style.display = "block";
       });
       j++;
-    }
-    // 반장의 아이디어입니다
+    };
+
+    for (var _i2 = (currentPage - 1) * 12; _i2 < board.length; _i2++) {
+      _loop3(_i2);
+    } // 반장의 아이디어입니다
     // titClass = document.querySelectorAll('.tit');
     // console.log(titClass)
     // titClass.forEach((a) => {
-
     //     a.addEventListener("click", function () {
     //         titleCon.style.display = "block";
     //     })
     // })
+
   } else {
-    let j = 0;
-
+    var _j = 0;
     subContainer.innerHTML = "";
-    for (let i = (currentPage - 1) * 12; i < currentPage * 12; i++) {
-      let _list = document.createElement("div");
-      _list.classList.add("list");
-      let numElement = document.createElement("div");
-      numElement.className = "num";
-      numElement.innerHTML = board[i].num;
 
-      //       let titElement = document.createElement("div");
+    for (var _i3 = (currentPage - 1) * 12; _i3 < currentPage * 12; _i3++) {
+      var _list = document.createElement("div");
+
+      _list.classList.add("list");
+
+      var numElement = document.createElement("div");
+      numElement.className = "num";
+      numElement.innerHTML = board[_i3].num; //       let titElement = document.createElement("div");
       //       titElement.className = "tit";
       //       titElement.innerHTML = board[i].title;
-
       //       let writerElement = document.createElement("div");
       //       writerElement.className = "writer";
       //       writerElement.innerHTML = board[i].writer;
-
       //       // let viewElement = document.createElement("div");
       //       // viewElement.className = "view";
       //       // viewElement.innerHTML = board[i].view
-
       //       _list.append(numElement);
       //       _list.append(titElement);
       //       _list.append(writerElement);
@@ -286,59 +294,63 @@ const changePage = () => {
       //     }
       //   }
       // };
-
       // //----active---
 
       document.querySelector("#pre").addEventListener("click", function () {
         currentPage -= 1;
+
         if (currentPage < 1) {
           currentPage = 1;
         }
+
         document.querySelector(".active").classList.remove("active");
         document.querySelector("#page" + currentPage).classList.add("active");
         changePage();
       });
       document.querySelector("#next").addEventListener("click", function () {
         currentPage += 1;
+
         if (currentPage > Math.floor(board.length / 12) + 1) {
           currentPage = Math.floor(board.length / 12) + 1;
         }
+
         document.querySelector(".active").classList.remove("active");
         document.querySelector("#page" + currentPage).classList.add("active");
         changePage();
-      });
-
-      //---거래소에서 로그아웃을 누르면 처음main으로 넘어가게 하는함수
+      }); //---거래소에서 로그아웃을 누르면 처음main으로 넘어가게 하는함수
       // 관리자일때
+
       console.log(JSON.parse(localStorage.getItem("nowLogin")));
+
       if (JSON.parse(localStorage.getItem("nowLogin")).isAdmin == true) {
         document.getElementById("logOut").innerHTML = "LogOut";
         document.getElementById("myPage").innerHTML = "Admin";
-        let logOut = document.getElementById("logOut");
+        var logOut = document.getElementById("logOut");
         logOut.href = "../main/main.html";
-        let admin = document.getElementById("myPage");
+        var admin = document.getElementById("myPage");
         console.log(admin);
         admin.href = "../admin/admin.html";
+
         logOut.onclick = function () {
           window.localStorage.removeItem("nowLogin");
-        };
-        // 회원일때
-      } else if (
-        JSON.parse(localStorage.getItem("nowLogin")).isAdmin == false
-      ) {
+        }; // 회원일때
+
+      } else if (JSON.parse(localStorage.getItem("nowLogin")).isAdmin == false) {
         document.getElementById("logOut").innerHTML = "로그아웃";
         document.getElementById("myPage").innerHTML = "My page";
-        let logOut = document.getElementById("logOut");
-        logOut.href = "../main/main.html";
-        let mypage = document.getElementById("myPage");
+
+        var _logOut = document.getElementById("logOut");
+
+        _logOut.href = "../main/main.html";
+        var mypage = document.getElementById("myPage");
         mypage.href = "../mypage/myPage.html";
-        logOut.onclick = function () {
+
+        _logOut.onclick = function () {
           window.localStorage.removeItem("nowLogin");
         };
-      } else {
-      }
-      //---q&a .수정버튼 만들기
+      } else {} //---q&a .수정버튼 만들기
       // 삭제버튼
+
     }
   }
 };
